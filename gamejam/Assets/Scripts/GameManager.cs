2031689df3d4516b7bd857538d84f66338b37e4f
dnwls6102 <dnwls6102@naver.com>
2024-04-27 11:59:46 +0900

    시간 디버깅 구현

--- gamejam/Assets/Scripts/GameManager.cs
+++ gamejam/Assets/Scripts/GameManager.cs
@@ -11,9 +11,15 @@ public class GameManager : MonoBehaviour
     public float GameTime = 5.0f;
     public Slider TimeBar;
 
+    private string[] sampleStringArray;
+    private bool firstDish;
+    private bool secondDish;
+    private bool thirdDish;
+
     void Awake()
     {
         MaxTime = GameTime;
+        sampleStringArray = new string[] { "쌀밥주세요", "짜장면주세요", "피자주세요" };
     }
 
     // Update is called once per frame
@@ -34,5 +40,19 @@ public class GameManager : MonoBehaviour
         {
             TimeBar.value = GameTime / MaxTime;
         }
+    }
+
+    public void CorrectDebug()
+    {
+        GameTime += 1.0f;
+        if (GameTime > MaxTime)
+        {
+            MaxTime = GameTime;
+        }
+    }
+
+    public void WrongDebug()
+    {
+        GameTime -= 1.0f;
     }
 }

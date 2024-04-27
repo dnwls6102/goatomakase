2bf400efd89b87a1b33f0a9d6cf2a157edf6139e
dnwls6102 <dnwls6102@naver.com>
2024-04-27 11:18:31 +0900

    타임아웃 구현 / 체력 겸 시간 바 구현

--- gamejam/Assets/Scripts/GameManager.cs
+++ gamejam/Assets/Scripts/GameManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager gm;
+    public float MaxTime;
+    public float GameTime = 5.0f;
+    public Slider TimeBar;
+
+    void Awake()
     {
-        
+        MaxTime = GameTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameTime -= Time.deltaTime;
+        CheckTime();
+        if (GameTime < 0)
+        {
+            Debug.Log("Time Over");
+            Time.timeScale = 0f;
+        }
+    }
+
+    void CheckTime()
+    {
+        if (TimeBar != null)
+        {
+            TimeBar.value = GameTime / MaxTime;
+        }
     }
 }

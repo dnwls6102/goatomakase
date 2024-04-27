9df1cee9800ba74e0ea5319056805e2c9c2db177
김민겸(MinKyum Kim) <minemin7@g.skku.edu>
2024-04-27 16:25:06 +0900

    a

--- gamejam/Assets/Scripts/Ingredient.cs
+++ gamejam/Assets/Scripts/Ingredient.cs
@@ -7,10 +7,12 @@ public class Ingredient : MonoBehaviour
     Camera cam;
     Vector2 pre_position;
     GameObject board;
+    SpriteRenderer spriteRenderer;
     private void Awake()
     {
         cam = Camera.main;
         board = GameObject.Find("board");
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); 
     }
     void Update()
     {
@@ -19,7 +21,9 @@ public class Ingredient : MonoBehaviour
 
     private void OnMouseDown()
     {
+
         pre_position = transform.position;
+        spriteRenderer.enabled = true;
 
     }
     private void OnMouseDrag()
@@ -34,12 +38,22 @@ public class Ingredient : MonoBehaviour
         Vector2 mouse_position = Input.mousePosition;
         mouse_position = cam.ScreenToWorldPoint(mouse_position);
         RaycastHit2D hit = Physics2D.Raycast(mouse_position, Vector3.down, 1, LayerMask.GetMask("table"));
+        
         if(hit.collider != null)
         {
-            hit.collider.gameObject.GetComponent<Board>().AddIngredient(this);
+            if (hit.collider.gameObject.GetComponent<Board>() != null)
+            {
+                hit.collider.gameObject.GetComponent<Board>().AddIngredient(this);
+            }
+            else if (hit.collider.gameObject.GetComponent<Cooker_SpriteChanger>() != null)
+            {
+                hit.collider.gameObject.GetComponent<Cooker_SpriteChanger>().ChangeToCookingSprite(0);
+            }
+            
         }
 
         transform.position = pre_position;
+        spriteRenderer.enabled = false;
     }
 
 }
